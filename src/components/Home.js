eb313e17355bb69f5709e819df5f34edde0dd5f4
tomy.dev <xxx.d1231@gmail.com>
2025-03-09 10:50:33 +0900

    React Router DOMでルーティング作成まで

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <h2>Home</h2>
+      <Link to="/login">ログインページへ</Link>
+    </>
+  );
+};
+
+export default Home;
