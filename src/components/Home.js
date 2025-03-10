395100486fbb3d9f33b5c5fdb2deedccbdeabc08
tomy.dev <xxx.d1231@gmail.com>
2025-03-10 11:27:47 +0900

    firebaseでデータベースの内容を取得して表示まで

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  // firebaseからデータを取得する(初回マウント時のみのため[])
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+
+      setPostList(
+        data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      );
+    };
+
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
-      <article className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
-        <div className="postTextContainer">
-          <p>
-            今はReactの学習中です。「吾輩は今日も縁側で昼寝をしながら、主人の苦々しい顔で論文を書いている様子を冷ややかに観察していた。書生は相変わらず無骨な様子で庭を掃いている。どうやら主人は近頃珍しく研究に没頭しているようだが、いかにも自惚れが強く見苦しい。その癖何も新しいことは書いていない。まったく人間というものは不可思議な生き物である。」「吾輩は今日も縁側で昼寝をしながら、主人の苦々しい顔で論文を書いている様子を冷ややかに観察していた。書生は相変わらず無骨な様子で庭を掃いている。どうやら主人は近頃珍しく研究に没頭しているようだが、いかにも自惚れが強く見苦しい。その癖何も新しいことは書いていない。まったく人間というものは不可思議な生き物である。」TypeScriptこそ至高
-          </p>
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@tommy</h3>
-          <button>削除</button>
-        </div>
-      </article>
+      {postList.map((post) => {
+        return (
+          <article key={post.id} className="postContents">
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">
+              <p>{post.postText}</p>
+            </div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </article>
+        );
+      })}
     </div>
   );
 };
